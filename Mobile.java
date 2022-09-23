c5b0c05616668e2c1d6de873578c10582f652803
unknown <manohar9.javadeveloper@gmail.com>
2022-09-23 20:29:59 +0530

    this is my dto java file of mobile table

---
+++ Mobile.java
@@ -0,0 +1,58 @@
+package jdbc_maven;
+
+public class Mobile {
+	private int id;
+	private String name;
+	private String brand;
+	private String color;
+	private double price;
+	private String ram;
+	private String manufacture;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getRam() {
+		return ram;
+	}
+	public void setRam(String ram) {
+		this.ram = ram;
+	}
+	public String getManufacture() {
+		return manufacture;
+	}
+	public void setManufacture(String manufacture) {
+		this.manufacture = manufacture;
+	}
+	
+	
+	
+	
+
+}
